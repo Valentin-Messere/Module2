df1429843e6e63759253ebbf5aab938f42290508
Valentin <messerevalentinn@gmail.com>
2025-10-03 17:40:18 +0000

    V2

--- exercice1.py
+++ exercice1.py
@@ -1,8 +1,10 @@
 fav_fruits = ["Apple", "Peach", "Orange"]
-new_fav_fruits = fav_fruits.copy()
 fav_fruits.append("Banana")
-fav_fruits.remove("Orange")
+fav_fruits.remove("Peach")
+# fav_fruits.pop(1)
 fav_fruits.sort()
 
 print(fav_fruits)
+
+new_fav_fruits = fav_fruits[0:4]
 print(new_fav_fruits)
